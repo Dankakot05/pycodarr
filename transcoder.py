82b813300449d9fc560973979c67090f1bec4c85
Hex600 <hex@hexmc.xyz>
2021-03-19 21:05:10 +0000

    added transcoding

--- transcoder.py
+++ transcoder.py
@@ -33,7 +33,7 @@ extensions = (
 
 
 def main():
-    file_directory = (r"")
+    file_directory = (r" ")
     bitrate = 10000000
     video_files = file_search(file_directory)
     print(video_files)
