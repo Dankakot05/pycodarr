71b810c8af663d7ab9706130daf31848a53a5022
Dankakot05 <67697592+Dankakot05@users.noreply.github.com>
2021-03-19 11:11:13 +0000

    Added File

---
+++ transcoder.py
@@ -0,0 +1,25 @@
+from os import walk
+from pkg_resources import cleanup_resources
+from pymediainfo import MediaInfo
+from pprint import pprint
+
+# some random libraries that are used I don't know why
+
+file_direct = (r"C:\Users\Daniil Koterov\Desktop\Test")
+
+# sets the file directory to be used to make changing directories easier
+
+_, _, filenames = next(walk(file_direct))
+
+# saves a list of all files in the directory specified
+
+for x in range(len(filenames)):
+    fileInfo = MediaInfo.parse(fr"{file_direct}\{filenames[x]}")
+    
+    # checks if any files in the directory are video files
+    
+    for track in fileInfo.tracks:
+        if track.track_type == "Video":
+            extension = ((filenames[x])[len(filenames[x])-4:len(filenames[x])])
+            print(extension)
+            
