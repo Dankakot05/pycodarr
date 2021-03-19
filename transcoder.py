c0911f22d853c859e8e292b4c251154c93cf9265
Dankakot05 <67697592+Dankakot05@users.noreply.github.com>
2021-03-19 13:48:22 +0000

    f**k

--- transcoder.py
+++ transcoder.py
@@ -1,3 +1,4 @@
+from os import walk
 from pymediainfo import MediaInfo
 from pprint import pprint
 import glob
@@ -5,13 +6,15 @@ import glob
 # some random libraries that are used I don't know why
 def main():
     file_directory = (r"")
-    extensions = ["mp4"]
-    print(video_search(file_directory, extensions))
+    print(video_search(file_directory))
 # searches recursivly for all file extensions in variable 'extensions'
     
-def video_search(file_direct, extensions):
+def video_search(file_direct):
     files = []
+    extensions = ["mp4"]
     for x in range(len(extensions)):
         temp = glob.glob(file_direct + f"/**/*.{extensions[x]}", recursive = True)
+        files.extend(temp)
+    return files
                 
 main()
